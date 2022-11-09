4eac427353cad0c05962507de68ce71bc031250a
Rayan Master <mrrayanmaster@gmail.com>
2022-11-09 21:49:54 +1100

    Finished single round

--- script.js
+++ script.js
@@ -1,7 +1,45 @@
-let computerOptions = ["rock", "paper", "scissors"];
+// Computer Options for opponent
 
-let computerOutput = function computerChoice() {
-    return computerOptions[Math.floor(Math.random() * 3)];
+let computerChoices = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+    return computerChoices[Math.floor(Math.random() * 3)];
 }
 
-console.log(computerOutput())
+console.log(`This is my computerChoice: ${getComputerChoice()}`)
+
+// Player Selecting options
+
+let userPrompt = prompt("Do you choose Rock, Paper, or Scissors?");
+
+function playerSelection() {
+    return userPrompt.toLowerCase()
+}
+
+console.log(`This is my player choice: ${playerSelection()}`)
+// play a round function
+
+computerSelection = getComputerChoice();
+
+
+function playRound(playerSelection, computerSelection) {
+
+    if (playerSelection == "rock" && computerSelection == "rock") {
+
+        console.log("you tied!")
+
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
+
+        console.log("you lost!")
+
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+
+        console.log("you won")
+    } else {
+        console.log("working")
+    }
+
+
+}
+
+playRound(userPrompt, computerSelection)
